be4a20fe670646271157941faae43db7d690a89b
HHolte <hahol@equinor.com>
2023-12-03 14:52:05 +0100

    Update dataloader

--- data_downloader.py
+++ data_downloader.py
@@ -1,5 +1,5 @@
 from aocd import get_data
-from os import getenv
+from os import getenv, makedirs
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -8,18 +8,15 @@ SESSION_TOKEN = getenv("SESSION_TOKEN")
 
 def download_and_save_data(day: int, year: int):
     data = get_data(SESSION_TOKEN, day, year)
-    save_data_to_file(day, data, data)
+    save_data_to_file(day, data)
 
 
-def save_data_to_file(day: int, example_data, test_data):
-    example_file_name = get_file_name(day, "example")
+def save_data_to_file(day: int, test_data):
     test_file_name = get_file_name(day, "test")
-
-    example_file = open(example_file_name, "w+")
+    makedirs(f"day{day}", exist_ok=True)
     test_file = open(test_file_name, "w+")
-
-    example_file.write(example_data)
     test_file.write(test_data)
+    test_file.close()
 
 
 def get_file_name(day: int, dataset: str):
@@ -27,4 +24,4 @@ def get_file_name(day: int, dataset: str):
 
 
 if __name__ == "__main__":
-    download_and_save_data(1, 2023)
+    download_and_save_data(2, 2023)
